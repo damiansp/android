8693efe01599967b2431fe0bb2f329ad96beb48b
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2024-09-20 13:21:18 -0700

    add rows with buttons

--- developer/01Essentials/todoList/app/src/main/java/com/example/todolist/MainActivity.kt
+++ developer/01Essentials/todoList/app/src/main/java/com/example/todolist/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -13,7 +17,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.todolist.ui.theme.TodoListTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,11 +27,23 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TodoListTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp(modifier = Modifier.fillMaxSize())
+            }
+        }
+    }
+}
+
+
+@Composable
+fun MyApp(
+    modifier: Modifier = Modifier, names: List<String> = listOf("World", "Android", "Compose")
+) {
+    Surface(
+            modifier = modifier,
+            color = MaterialTheme.colorScheme.background) {
+        Column(modifier.padding(vertical = 4.dp)) {
+            for (name in names) {
+                Greeting(name = name)
             }
         }
     }
@@ -33,16 +51,25 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface(
+            color = MaterialTheme.colorScheme.primary,
+            modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = modifier.weight(1f)) {
+                Text(text = "Hello,")
+                Text(text = "$name!")
+            }
+            ElevatedButton(onClick = { /*TODO*/ }) {
+                Text("Show more")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TodoListTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
