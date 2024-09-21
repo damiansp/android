7ba08f4125812843a2ad210ec125c7910744e5da
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2024-09-20 22:15:35 -0700

    add landing page

--- developer/01Essentials/todoList/app/src/main/java/com/example/todolist/MainActivity.kt
+++ developer/01Essentials/todoList/app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,22 +1,28 @@
 package com.example.todolist
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,7 +44,34 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun MyApp(
+fun MyApp(modifier: Modifier = Modifier) {
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    Surface(modifier) {
+        if (shouldShowOnboarding) {
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        } else {
+            Greetings()
+        }
+    }
+}
+
+
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit, modifier: Modifier = Modifier) {
+    Column(
+            modifier = modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+        Text("Welcome to Basics Codelab!")
+        Button(modifier = Modifier.padding(vertical = 24.dp), onClick = onContinueClicked) {
+            Text("Continue")
+        }
+    }
+}
+
+
+@Composable
+fun Greetings(
     modifier: Modifier = Modifier, names: List<String> = listOf("World", "Android", "Compose")
 ) {
     Surface(
@@ -60,7 +93,9 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             color = MaterialTheme.colorScheme.primary,
             modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = modifier.weight(1f).padding(bottom = extraPadding)) {
+            Column(modifier = modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)) {
                 Text(text = "Hello,")
                 Text(text = "$name!")
             }
@@ -71,10 +106,20 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     }
 }
 
-@Preview(showBackground = true)
+
+@Preview
 @Composable
-fun GreetingPreview() {
+fun MyAppPreview() {
     TodoListTheme {
-        MyApp()
+        MyApp(Modifier.fillMaxSize())
+    }
+}
+
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    TodoListTheme {
+        OnboardingScreen(onContinueClicked = {})
     }
 }
