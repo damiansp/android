8bdaf2ba981c4abd7023b341782652783348e5cf
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2024-10-05 16:53:37 -0700

    add favorites collection card

--- developer/02BasicLayouts/layouts/app/src/main/java/com/example/layouts/MainActivity.kt
+++ developer/02BasicLayouts/layouts/app/src/main/java/com/example/layouts/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.layouts
 import android.graphics.drawable.Drawable
 import android.graphics.drawable.Icon
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Im
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,18 +11,21 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -66,7 +70,6 @@ fun SearchBar(modifier: Modifier = Modifier) {
             .heightIn(min = 56.dp))
 }
 
-
 @Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
 @Composable
 fun SearchBarPreview() {
@@ -82,14 +85,15 @@ fun AlignYourBodyElement(
             painter = painterResource(drawable),
             contentDescription = null,
             contentScale = ContentScale.Crop,
-            modifier = Modifier.size(88.dp).clip(CircleShape))
+            modifier = Modifier
+                .size(88.dp)
+                .clip(CircleShape))
         Text(
             text = stringResource(text),
             modifier = Modifier.paddingFromBaseline(top = 24.dp, bottom = 8.dp),
             style = MaterialTheme.typography.bodyMedium)
     }
 }
-
 
 @Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
 @Composable
@@ -98,6 +102,36 @@ fun AlignYourBodyElementPreview() {
         AlignYourBodyElement(
             drawable = R.drawable.ab1_inversions,
             text = R.string.ab1_inversions,
+            modifier = Modifier.padding(8.dp))
+    }
+}
+
+
+@Composable
+fun FavoriteCollectionCard(
+        @DrawableRes drawable: Int, @StringRes text: Int, modifier: Modifier = Modifier) {
+    Surface(shape = MaterialTheme.shapes.medium, modifier = modifier) {
+        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.width(255.dp)) {
+            Image(
+                painter = painterResource(drawable),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.size(80.dp))
+            Text(
+                text = stringResource(text),
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.padding(horizontal = 16.dp))
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+@Composable
+fun FavoriteCollectionCardPreview() {
+    LayoutsTheme {
+        FavoriteCollectionCard(
+            text = R.string.fc2_nature_meditations,
+            drawable = R.drawable.fc2_nature_meditations,
             modifier = Modifier.padding(8.dp))
     }
 }
