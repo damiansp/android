9b59387339632f0fae6880138f00d0b8f173ad62
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2024-10-13 15:47:13 -0700

    add slider

--- composeEssentials/demo/app/src/main/java/com/example/composedemo/MainActivity.kt
+++ composeEssentials/demo/app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -7,12 +7,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composedemo.ui.theme.ComposeDemoTheme
 
@@ -33,4 +35,22 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DemoText(msg: String, fontSize: Float) {
     Text(text = msg, fontSize = fontSize.sp, fontWeight = FontWeight.Bold)
+}
+
+@Preview
+@Composable
+fun DemoTextPreview() {
+    ComposeDemoTheme {
+        DemoText(msg = "Welcome to Android", fontSize = 12f)
+    }
+}
+
+
+@Composable
+fun DemoSlider(position: Float, onPositionChange: (Float) -> Unit) {
+    Slider(
+        modifier = Modifier.padding(10.dp),
+        valueRange = 20f..38f,
+        value = position,
+        onValueChange = { onPositionChange(it) })
 }
