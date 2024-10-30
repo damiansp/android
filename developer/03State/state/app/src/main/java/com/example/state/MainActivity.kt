3fd2e4095e9f3c3394a90979573b417e4e18ee81
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2024-10-30 11:59:10 -0700

    upate state of water count

--- developer/03State/state/app/src/main/java/com/example/state/MainActivity.kt
+++ developer/03State/state/app/src/main/java/com/example/state/MainActivity.kt
@@ -5,13 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -42,8 +50,17 @@ fun WellnessScreen(modifier: Modifier = Modifier) {
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count = 0
-    Text(text = "\nYou've had $count glasses.", modifier = modifier.padding(16.dp))
+    Column(modifier = modifier.padding(16.dp)) {
+        var count by remember { mutableStateOf(0) }
+        Text(text = "\nYou've had $count glasses.")
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) { Text("Add one") }
+    }
 }
 
+
+@Preview
+@Composable
+fun WaterCounterPreview() {
+    WaterCounter(Modifier)
+}
 
