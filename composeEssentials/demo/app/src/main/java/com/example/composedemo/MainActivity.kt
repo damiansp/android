794809a55f2f467e261aff2ae537e8e7a6f83896
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2024-10-30 20:40:10 -0700

    functional slider

--- composeEssentials/demo/app/src/main/java/com/example/composedemo/MainActivity.kt
+++ composeEssentials/demo/app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,9 +35,28 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeDemoTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
+                    DemoScreen()
                 }
             }
         }
+    }
+}
+
+
+@Composable
+fun DemoScreen() {
+    var sliderPosition by remember { mutableStateOf(20f) }
+    var handlePositionChange = { position: Float -> sliderPosition = position }
+    Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxSize()) {
+        DemoText(msg = "Welcome to Compose", fontSize = sliderPosition)
+        Spacer(modifier = Modifier.height(150.dp))
+        DemoSlider(position = sliderPosition, onPositionChange = handlePositionChange)
+        Text(
+            text = sliderPosition.toInt().toString() + "sp",
+            style = MaterialTheme.typography.headlineMedium)
     }
 }
 
