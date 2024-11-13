b6bbab9e5abb2033f5e24b3307724460ed596380
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2024-11-13 11:02:35 -0800

    remember state on rotation

--- developer/03State/state/app/src/main/java/com/example/state/MainActivity.kt
+++ developer/03State/state/app/src/main/java/com/example/state/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -52,7 +53,7 @@ fun WellnessScreen(modifier: Modifier = Modifier) {
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember { mutableStateOf(0) }
+        var count by rememberSaveable { mutableStateOf(0) }
         if (count > 0) {
             // Text present only if button pressed at least once
             Text(text = "\nYou've had $count glasses.")
