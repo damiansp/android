78fb0e878ebe4c9d74c56bfa385cac70104a219b
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2025-02-27 21:47:30 -0800

    reinit core composables w up-to-date dependencies

---
+++ completeGuide/1_coreComposables/core_composables/app/src/main/java/com/example/core_composables/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.example.core_composables
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.core_composables.ui.theme.Core_composablesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Core_composablesTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun SimpleText() {
+    Text(text="Hello, Jetpack", fontSize=24.sp, fontWeight=FontWeight.Bold, color=Color.Blue)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SimpleTextPreview() {
+    Core_composablesTheme {
+        SimpleText()
+    }
+}
+
+
+@Composable
+fun StyledText() {
+    val myStyle = TextStyle(
+        fontFamily = FontFamily.Serif, fontSize = 18.sp, fontWeight = FontWeight.SemiBold)
+    Text(
+        text="Styled Text",
+        style=myStyle,
+        modifier = Modifier.padding(16.dp).clickable{ /* TODO */ })
+}
+
+@Preview(showBackground = true)
+@Composable
+fun StyledTextPreview() {
+    Core_composablesTheme {
+        StyledText()
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Core_composablesTheme {
+        Greeting("Android")
+    }
+}
