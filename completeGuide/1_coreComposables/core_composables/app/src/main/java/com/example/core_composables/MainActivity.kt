0f6623f2221038ab4e7a658123a1a8725e0742d6
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2025-02-28 21:05:19 -0800

    Add images, buttons

--- completeGuide/1_coreComposables/core_composables/app/src/main/java/com/example/core_composables/MainActivity.kt
+++ completeGuide/1_coreComposables/core_composables/app/src/main/java/com/example/core_composables/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.example.core_composables
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -59,7 +63,9 @@ fun StyledText() {
     Text(
         text="Styled Text",
         style=myStyle,
-        modifier = Modifier.padding(16.dp).clickable{ /* TODO */ })
+        modifier = Modifier
+            .padding(16.dp)
+            .clickable { /* TODO */ })
 }
 
 @Preview(showBackground = true)
@@ -67,6 +73,34 @@ fun StyledText() {
 fun StyledTextPreview() {
     Core_composablesTheme {
         StyledText()
+    }
+}
+
+
+@Composable
+fun Images() {
+    //Image(painter = painterResource(id = R.drawable.my_image), contentDescription = "My Image")
+    /*
+    val bitmap = BitmapFactory.decodeResource(resources, R.drawable.my_image)
+    Image(bitmap = bitmap.asImageBitmap(), contentDescription = "My Image")
+    */
+    val imageUri = "https://www.example.com/my_image.jpg"
+    // AsyncImage(model = imageUri, contentDescription = "My Image")
+}
+
+
+@Composable
+fun MyButton() {
+    Button(onClick = { /* TODO */ }) {
+        Text("Click Me")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MyButtonPreview() {
+    Core_composablesTheme {
+        MyButton()
     }
 }
 
