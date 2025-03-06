ec0110ed930cadac13fea69ba25465085b442b91
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2025-03-06 11:04:13 -0800

    +layou exs

--- completeGuide/1_coreComposables/core_composables/app/src/main/java/com/example/core_composables/MainActivity.kt
+++ completeGuide/1_coreComposables/core_composables/app/src/main/java/com/example/core_composables/MainActivity.kt
@@ -2,17 +2,25 @@ package com.example.core_composables
 
 import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonElevation
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -91,7 +99,12 @@ fun Images() {
 
 @Composable
 fun MyButton() {
-    Button(onClick = { /* TODO */ }) {
+    val buttonColors = ButtonDefaults.buttonColors(Color.Blue, Color.White)
+    Button(
+        onClick = { /* Toast.makeText(context, "Clicked", Toast.LENGTH_SHORT).show() */ },
+        modifier = Modifier.padding(8.dp),
+        colors = buttonColors
+    ) {
         Text("Click Me")
     }
 }
@@ -103,6 +116,57 @@ fun MyButtonPreview() {
         MyButton()
     }
 }
+
+
+@Composable
+fun MyCol() {
+    Column {
+        Text("Item 1")
+        Text("Item 2")
+        // Image(painter = painterResource(id = R.drawable.my_image), contentDescription = null)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MyColPreview() {
+    MyCol()
+}
+
+
+@Composable
+fun MyRow() {
+    Row {
+        Text("Item 1")
+        Button(onClick = { /* ... */ }) { Text("Click!") }
+        Text("Item 2")
+        // Image(painter = painterResource(id = R.drawable.my_image), contentDescription = null)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MyRowPreview() {
+    MyRow()
+}
+
+
+@Composable
+fun MyBox() {
+    Box {
+        // Image(painter = painterResource(id = R.drawable.my_image), contentDescription = null)
+        Button(onClick = { /* ... */ }) { Text("Click!") }
+        Text("Item 1", modifier = Modifier.align(Alignment.Center))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MyBoxPreview() {
+    MyBox()
+}
+
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
